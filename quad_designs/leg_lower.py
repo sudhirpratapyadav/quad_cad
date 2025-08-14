a473f2392840483837ba3f405677a8d7bcc70883
sudhirpratapyadav <sudhirpratapyadav@gmail.com>
2025-08-14 14:56:27 +0530

    lower leg design updated, 2nd part of leg loft with rect->square->circle

--- quad_designs/leg_lower.py
+++ quad_designs/leg_lower.py
@@ -1,58 +1,104 @@
-"""
-Quadruped Lower Leg - Motor Attachment Disc
-
-This script creates a parametric disc for attaching to a motor rotor.
-The disc features:
-- Main circular disc with configurable radius
-- 8 mounting holes equally spaced on a pitch circle
-- Configurable hole size and position
-- Adjustable thickness for extrusion
-
-Modify the parameters below to customize the design.
-"""
-
 import cadquery as cq
 import math
 
-# Motor rotor coupler params
-rotor_radius = 14.0  # Radius of the main rotor (28 mm diameter)
-hole_center_distance = 11.0  # Distance from rotor center to hole centers (PCD) (22 mm diameter)
-hole_radius = 1.25  # Radius of each mounting hole (2.5 mm diameter)
-thickness = 5.0  # Thickness of the disc
-num_holes = 8  # Number of mounting holes
-axle_hole_radius = 5.0  # Radius of center hole for motor axle (10 mm diameter)
+### ------------PARAMETERS OF LOWER LEG-------------
 
-# Exntension params
-L = 60.0
-W = 20.0
+HOLE_TOLERANCE = 0.25  # Tolerance for hole dimensions (in radius) due to 3D printing
+
+## Motor rotor coupler params (Based on Actual Motor)
+rotor_disc_radius = 14.0  # Radius of the motor rotor disc (28 mm diameter)
+rotor_disc_holes_pcd = 11.0  # Distance from rotor center to hole centers (PCD) (22 mm diameter)
+rotor_disc_holes_radius = 1.25 + HOLE_TOLERANCE  # Radius of each mounting hole (2.5 mm diameter)
+rotor_disc_holes_num = 8  # Number of mounting holes
+rotor_disc_axle_hole_radius = 5 + HOLE_TOLERANCE  # Radius of center hole for motor axle (10 mm diameter)
+
+## Parameters of leg_part_1
+rotor_coupler_thickness = 5.0  # Thickness of the coupler disc and extension attached to rotor disc
+L1 = 90.0 # Length of leg_part_1: first part of leg (attached to motor rotor)
+WIDTH = 20.0 # Width of the leg_part_1
+motor_coupler_gap_half = 52/2 # Breadth of leg_part_1
+
+# sketch parameters
 offset = 0
-bottom_offset = 3
+angle_deg = 6
+delta_back = math.tan(angle_deg*math.pi/180)*WIDTH/2
+bottom_offset = 7
 
+## Parameters for leg_part_2
+L2 = 110 # Length of leg_part_2 (from end of leg_part_1 till sphere center)
+rect_to_square_dist = 60
+square_to_circle_dist = L2-rect_to_square_dist
+start_rect_breadth = 2*(motor_coupler_gap_half+rotor_coupler_thickness) # breadth of the rectangular crossection at start of leg_part_2 (width is as same WIDTH)
+midlle_square_size = 10 # size of the square cross section at the mid part of leg_part_2
+end_circle_radius = 6 # radius of the circular cross-section at the end of leg_part_2
+
+## End sphere params
+sphere_radius = 10
+sphere_cut_dist = math.sqrt(sphere_radius**2 - end_circle_radius**2) # distance from center of sphere where we need to cut the sphere to attach it to circular end
+sphere_to_rotor_dist = L1+L2+sphere_cut_dist # Distance between rotor coupler center to sphere center
+
+Total_LENGTH = L1 + L2 + sphere_cut_dist + sphere_radius
+print("Total Length = ",Total_LENGTH)
+### --------------------------------------------------------------
+
+# Sketch of leg_part_1
 sketch = (
     cq.Sketch()
-    .arc((0, 0), rotor_radius, 90.0, 90.0)
-    .spline([(0, rotor_radius), (25,W/2+offset)], [(1, 0), (1, 0)], False) 
-    .segment((25,W/2+offset),(L,W/2+offset))
-    .segment((L,W/2+offset),(L,-W/2+offset))
-    .segment((L,-W/2+offset),(25,-W/2+offset))
-    .spline([(25,-W/2+offset), (0, -rotor_radius-bottom_offset), (-rotor_radius, 0)], [(-1.0, 0), (-1.0, 0), (0, 1.0)], False)
+    .arc((0, 0), rotor_disc_radius, 90.0, 90.0)
+    .spline([(0, rotor_disc_radius), (25,WIDTH/2+offset)], [(1, 0), (1, 0)], False) 
+    .segment((25,WIDTH/2+offset),(L1+delta_back,WIDTH/2+offset))
+    .segment((L1+delta_back,WIDTH/2+offset),(L1-delta_back,-WIDTH/2+offset))
+    .segment((L1-delta_back,-WIDTH/2+offset),(25,-WIDTH/2+offset))
+    .spline([(25,-WIDTH/2+offset), (0, -rotor_disc_radius-bottom_offset), (-rotor_disc_radius, 0)], [(-1.0, 0), (-1.0, 0), (0, 1.0)], False)
 )
-
-# Add center hole for motor axle at origin
-sketch = sketch.arc((0.0, 0.0), axle_hole_radius, 0.0, 360.0)
-
-# Add holes at equally spaced positions on the pitch circle
-for i in range(num_holes):
-    angle = i * (360.0 / num_holes)  # Degrees
-    x_pos = hole_center_distance * math.cos(math.radians(angle))
-    y_pos = hole_center_distance * math.sin(math.radians(angle))
-    sketch = sketch.arc((-x_pos, -y_pos), hole_radius, 0.0, 360.0)
-
-# Extrude the 2D profile to create the 3D disc with holes
+sketch = sketch.arc((0.0, 0.0), rotor_disc_axle_hole_radius, 0.0, 360.0)
+for i in range(rotor_disc_holes_num):
+    angle = i * (360.0 / rotor_disc_holes_num)  # Degrees
+    x_pos = rotor_disc_holes_pcd * math.cos(math.radians(angle))
+    y_pos = rotor_disc_holes_pcd * math.sin(math.radians(angle))
+    sketch = sketch.arc((-x_pos, -y_pos), rotor_disc_holes_radius, 0.0, 360.0)
 sketch = sketch.assemble()
+rotor_coupler_1 = cq.Workplane("XY").placeSketch(sketch).extrude(rotor_coupler_thickness).translate((0, 0, motor_coupler_gap_half)) 
+rotor_coupler_2 = cq.Workplane("XY").placeSketch(sketch).extrude(rotor_coupler_thickness).translate((0, 0, -motor_coupler_gap_half-rotor_coupler_thickness))
 
-rotor_coupler = cq.Workplane("XY").placeSketch(sketch).extrude(thickness)
+# Extra rotor rotor disc extrusion for tight fit
+rotor_disc_sketch = cq.Sketch().arc((0, 0), rotor_disc_radius, 0.0, 360.0).arc((0.0, 0.0), rotor_disc_axle_hole_radius, 0.0, 360.0)
+for i in range(rotor_disc_holes_num):
+    angle = i * (360.0 / rotor_disc_holes_num)  # Degrees
+    x_pos = rotor_disc_holes_pcd * math.cos(math.radians(angle))
+    y_pos = rotor_disc_holes_pcd * math.sin(math.radians(angle))
+    rotor_disc_sketch = rotor_disc_sketch.arc((-x_pos, -y_pos), rotor_disc_holes_radius, 0.0, 360.0)
+rotor_disc_sketch = rotor_disc_sketch.assemble()
+rotor_disc1 = cq.Workplane("XY").placeSketch(rotor_disc_sketch).extrude(1).translate((0, 0, motor_coupler_gap_half-1))
+rotor_disc2 = cq.Workplane("XY").placeSketch(rotor_disc_sketch).extrude(1).translate((0, 0, -motor_coupler_gap_half))
+
+# Leg part one combined
+leg_part_1 = rotor_coupler_1.union(rotor_coupler_2).union(rotor_disc1).union(rotor_disc2)
+
+# Leg part 2 
+start_rect = cq.Sketch().rect(start_rect_breadth, WIDTH).vertices().fillet(0.3)
+mid_square = cq.Sketch().rect(midlle_square_size, midlle_square_size).vertices().fillet(0.3)
+end_circle = cq.Sketch().circle(end_circle_radius)
+leg_part_2 = cq.Workplane().placeSketch(
+    start_rect, 
+    mid_square.moved(cq.Location(cq.Vector(0, 0, rect_to_square_dist))), 
+    end_circle.moved(cq.Location(cq.Vector(0, 0, rect_to_square_dist+square_to_circle_dist)))
+).loft().rotate((0,0,0), (0,1,0), 90).translate((L1, 0, 0))
+
+# End Sphere
+cutting_box = cq.Workplane("XY").box(sphere_radius*2, sphere_radius*2, sphere_radius*2).translate((-sphere_cut_dist-sphere_radius, 0, 0))
+sphere = cq.Workplane("XY").sphere(sphere_radius).cut(cutting_box).translate((sphere_to_rotor_dist, 0, 0))
+
+# Combine leg part 2 and sphere to rotate them together
+leg_part_23 = leg_part_2.union(sphere).rotate((L1,0,0), (L1,0,1), -angle_deg).translate((0, offset, 0))
+
+# Combine All leg parts
+lower_leg = leg_part_1.union(leg_part_23)
 
 # Export the result in multiple formats
-rotor_coupler.export("rotor_coupler.svg")
-rotor_coupler.export("rotor_coupler.step")
+lower_leg.export("../assets/quad_parts/lower_leg.svg")
+lower_leg.export("../assets/quad_parts/lower_leg.step")
+lower_leg.export("../assets/quad_parts/lower_leg.stl")
+
+# Alternative method using show_object for CQGI export
+# show_object(lower_leg)
